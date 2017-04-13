dae4bf6c54726ad3c2e36a7add90c443867c4858
Luc Flynn <lucnmsu@gmail.com>
2017-04-13 09:39:21 -0600

    Filled out persona, will need to redo use-case as content isn't correct.

--- epic/persona.php
+++ epic/persona.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Beta
- * Date: 4/13/17
- * Time: 8:58 AM
- */
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Persona</title>
+	</head>
+	<body>
+
+
+	<h1> Persona </h1>
+		<p> <strong> Name: </strong> Kim <br> <strong> Age </strong> 33 </p>
+
+		<p> <strong>Profession</strong> <br> Mid-Level engineer at an electronics company. Typically working in and dealing with monitoring systems of machinery and more often than she would like trouble shooting these systems to keep everything running. She has worked here for about 2 years and thinks of it more as a stepping stone to gain some skills in order to move on to a bigger start up where she can have more freedom to design embedded systems.</p>
+		<p> <strong>Technology</strong> <br> She used a mac, and has an iphone. Some of her more techie friends scoff at her for not choosing google but she enjoys the elegence and simplicity of the user interface of the iphone. She thinks it's the height of UX disign. She uses google cast to watch netflix and youtube at home on a 32" screen at the foot of her bed. She also has a 24" monitor at her desk at home in case she needs to work from home. She ejoys apple music but also has spotify. </p>
+		<p> <strong>Attitudes and Behaviors</strong> <br> Likes to think of herself as loving books but hasn't read in years due to social media addiction, spending 40-50 hours a week at work and trying to meet up with friends on top of that. She is up to date on game of thrones and westworld however. She goes to networking events but is uncomfortable there and perfers to network online using linkedIn and twitter. Although she doesn't post much. She is jealous more people don't understand embedded systems and how to program robots so she could effectively use github.  </p>
+		<p> <strong>Furstrations and Needs</strong> <br> She enjoys having small personal items that are unique, and useful. Form and functin are a big part of what she likes in products but she also likes for them to be visually appealing in their colors and design. Her needs are specific therefore she typically doesn't find exactly what she wants at retail shops in the mall or on amazon.   </p>
+		<p> <strong>Goals</strong> <br> She has high expectations and demands out of products because she feels that if she is paying money she should get exactly what she wants. This is why she loves a very bad etsy site because she can show off how great her taste is. She is frustrated that she cannot purchase more items but she was raised practical and understands she doesn't need a house full of things. </p>
+
+	</body>
+</html>
