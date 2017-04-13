068ad64ca9cf5eef8a64c95a4f75221c6f9c9049
Luc Flynn <lucnmsu@gmail.com>
2017-04-13 08:59:51 -0600

    Fixed use-case, created peresona

---
+++ epic/persona.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Beta
+ * Date: 4/13/17
+ * Time: 8:58 AM
+ */
--- epic/use-case.php
+++ epic/use-case.php
@@ -5,5 +5,5 @@
 		<title>Use Case</title>
 	</head>
 	<body> </body>
-	<p>A site you can favorite products</p>
+	<p>As a very bad etsy user I want to see products I like, and favorite them so I can come back later and drool over my great taste.</p>
 </html>
