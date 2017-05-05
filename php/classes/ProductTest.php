6c9daacfcfa46f0e8c408477e8f0163098ddc9f1
Luc Flynn <lucnmsu@gmail.com>
2017-05-05 09:13:02 -0600

    Reviewing work.

---
+++ php/classes/ProductTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+namespace home/lflynn7/data-design/productTest
+
+use Product.php
+
+require_once(Desktop/bootcamp/git/data-design/php/classes/Product.php);
+*/
+
+class ProductTest {
+
+    protected $profile = null;
+
+    protected $VALID_PROFILE_HASH;
+
+    protected $VALID_PROFILE_SALT;
+
+    protected $VALID_PRODUCT = "valid product";
+
+    protected $VALID_SUNRISEDATE = null;
+
+    protected $VALID_SUNSETDATE = null;
+
+}
+
